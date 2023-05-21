a29f0c539fc89a6064a7d707f0c62b5c2734dc23
minoli-g <minoliniroshani@gmail.com>
2023-05-21 20:56:38 +0530

    Load and display database records

--- lib/history.dart
+++ lib/history.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fit_buddy/db.dart';
+import 'package:fit_buddy/log.dart';
 
 class HistoryPage extends StatefulWidget{
   const HistoryPage({super.key, required this.title});
@@ -13,18 +15,43 @@ class _HistoryPageState extends State<HistoryPage>{
 
   // TODO - 
   // On opening the page, delete any records older than 2 weeks
-  // Load the remaining records and display in a nice graphical format
+  // Display loaded records in a nice graphical format
+
+  List<Log> allRecords = DatabaseAdapter.getAllRecords();
 
   @override
   Widget build(BuildContext context) {
-    
+
     return Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Your exercise history will appear here',
-            ),
-          ],
+          children: [
+            ListView.separated(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              itemCount: allRecords.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 50,
+                  color: Colors.grey,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(allRecords[index].walkTime.toString(), textAlign: TextAlign.center)
+                      ),
+                      Expanded(
+                        child: Text(allRecords[index].runTime.toString(), textAlign: TextAlign.center)
+                      ),
+                      Expanded(
+                        child: Text(allRecords[index].bicycleTime.toString(), textAlign: TextAlign.center)
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => const Divider(),
+            )
+           ],
         );
   }
 }
